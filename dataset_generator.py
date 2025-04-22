2bcc37ca27c6d2e2ca8ceb509ce1a8f2b7d79ec3
rkdesai7 <rykdesai@ucdavis.edu>
2025-04-22 11:35:37 -0700

    cleanie

--- dataset_generator.py
+++ dataset_generator.py
@@ -1,50 +1,70 @@
+import argparse
 import matplotlib
 import random
 import sys
 from setup.lib.korflab import readfasta
 
-#Real
-utr5 = {}
-cds = {}
-data = []
-window = int(sys.argv[3])
-for i in readfasta(sys.argv[1]):
-	name = i[0].split()[0]
-	w = -1*window
-	sequence = i[1][w:]
-	utr5[name] = sequence
-for i in readfasta(sys.argv[2]):
-	name = i[0].split()[0]
-	sequence = i[1][:3+window]
-	if sequence[:3] == "ATG": cds[name] = sequence
-for key, value in utr5.items():
-	if key in cds: data.append(key + "|" + utr5[key] + cds[key] + "\n")
-for i in data:
-	if i[5:8] != "ATG": data.remove(i)
-data = random.sample(data, 1000)
-with open('real_kozac.txt', 'w') as f:
-	counter = 0
-	for i in data:
-		f.write(i)
-print(counter)
+parser = argparse.ArgumentParser(description="Extracts kozac sequence data from 5' utr and cds .gz files from a specific organism")
+parser.add_argument("cds", type=str, help="Path to cds .gz file")
+parser.add_arugment("5utr", type=str, help="Path to 5' utr .gz file")
+parser.add_argument("--window", type=int, default=5, help="How many base pairs you want before and after the ATG in the kozac sequence")
+parser.add_argument("--size_real", type=int, default=1000, help="How many real kozac sequences you want")
+parser.add_argument("--size_fake", type=int, default=1000, help="How many fake kozac sequences you want")
 
-#Fake ATG from coding region
-false_data = []
-for i in readfasta(sys.argv[2]):
-	name = i[0].split()[0]
-	seq = i[1][3:]
-	for j in range(len(seq) - 4):
-		win = seq[j:j + 3]
-		if win == "ATG":
-			kozac = seq[j-window:j+3+window]
-			text = name + "|" + kozac + "\n"
-			false_data.append(text)
-false_data = random.sample(false_data, 1000)
-with open('fake_kozac.txt', 'w') as f:
-	for i in false_data:
-		f.write(i)
-		counter += 1
-print(counter)
+arg=parser.parse_args()
+
+def write_specified_size_to_file(data, file_name, size):
+	"""get specified size of data and write to a text file"""
+	for i in data:
+		if i[5:8] != "ATG": data.remove(i)
+		if len(i) != 3+(2*arg.window): data.remove(i)
+	data = random.sample(data, size)
+	with open(file_name, 'w') as f:
+		for i in data:
+			f.write(i)
+			
+def gen_real():
+	"""extracts real kozac sequences"""
+	utr5 = {}
+	cds = {}
+	data = []
+	
+	for i in readfasta(arg.5utr):
+		name = i[0].split()[0]
+		w = -1*arg.window
+		sequence = i[1][w:]
+		utrs5[name] = sequence
+	
+	for i in readfasta(arg.cds):
+		name = i[0].split()[0]
+		sequence = i[1][:3+arg.window]
+		if sequence[:3] == "ATG": cds[name] = sequence
+		
+	for key, value in utr5.items():
+		if key in cds: data.append(key + "|" + utr5[key] + cds[key] + "\n")
+		
+	write_specified_size_to_file(data, "real_kozac.txt", arg.size_real)
+	print(f"Real Kozac Data of Size {arg.size_real} Generated")
+	
+def gen_fake():
+	"""compiles fake kozac sequences (ATG codons not part of promoter)"""
+	data = []
+	
+	for i in readfasta(arg.cds):
+		name = i[0].split()[0]
+		seq = i[1][3:]
+		for j in range(len(seq) - 4):
+			win = seq[j:j+3]
+			if win == "ATG"
+				kozac = seq[j-arg.window:j+3+arg.window]
+				text = name + "|" + kozac + "\n"
+				data.append(text)
+	
+	write_specified_size_to_file(data, "fake_kozac.txt", arg.size_fake)
+	print(f"Fake Kozac Data of Size {arg.size_fake} Generated")
+
+gen_real()
+gen_fake()
 		
 
 				
