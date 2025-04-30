2997839223f7bc5b045fc3cfe38b49c6b0f0181d
rkdesai7 <rykdesai@ucdavis.edu>
2025-04-30 15:09:45 -0700

    uppies

--- train_nn.py
+++ train_nn.py
@@ -208,10 +208,8 @@ def gen_pwm(full_data):
 	for ind, i in enumerate(counts): 
 		total = sum(i)
 		for index, item in enumerate(i):
-            if total == 0:
-                counts[ind][index] = 0
-            else:
-                counts[ind][index] = item/total
+			if total == 0: counts[ind][index] = 0
+            else:          counts[ind][index] = item/total
 	return counts
 
 def pwm_true_frequencies():
